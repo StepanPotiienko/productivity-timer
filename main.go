7a0833139bb7c8f15f115a65cb2f0ce75719b161
StepanPotiienko <kotnexjieb@protonmail.com>
2024-12-07 14:35:14 +0200

    completely redesigned the app: removed quotes, added a stylish macos look to it

--- main.go
+++ main.go
@@ -6,7 +6,6 @@ import (
 	"net/http"
 	"os"
 
-	quote "github.com/StepanPotiienko/todo-productivity/quote"
 	"github.com/joho/godotenv"
 
 	"github.com/gin-gonic/gin"
@@ -19,23 +18,6 @@ func main() {
 		log.Println("Error loading .env file. Proceeding with the default values.")
 	}
 
-	router := gin.Default()
-
-	router.Static("/css", "template/css")
-	router.Static("/js", "template/js")
-	router.LoadHTMLGlob("template/*.html")
-
-	gin.SetMode(gin.ReleaseMode)
-
-	router.GET("/", func(c *gin.Context) {
-		var quote quote.Quote = quote.FetchQuote()
-
-		c.HTML(http.StatusOK, "index.html", gin.H{
-			"quote":  quote.HtmlCode,
-			"author": quote.Author,
-		})
-	})
-
 	port, exists := os.LookupEnv("PORT")
 
 	if !exists {
@@ -43,9 +25,21 @@ func main() {
 		port = "8080"
 	}
 
-	runAddress := fmt.Sprintf(":%s", port)
+	router := gin.Default()
 
-	log.Printf("Starting server on %s...", runAddress)
+	router.Static("/css", "template/css")
+	router.Static("/js", "template/js")
+	router.Static("/media", "template/media")
+	router.LoadHTMLGlob("template/*.html")
+
+	gin.SetMode(gin.ReleaseMode)
+
+	router.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.html", gin.H{})
+	})
+
+	runAddress := fmt.Sprintf(":%s", port)
+	defer log.Printf("Starting server on %s...", runAddress)
 
 	if err := router.Run(runAddress); err != nil {
 		log.Fatalf("Failed to start server: %v", err)
