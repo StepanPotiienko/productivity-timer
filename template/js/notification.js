a93d4dffb369926bb304dfa55c9a72193371e852
StepanPotiienko <kotnexjieb@protonmail.com>
2024-12-13 06:27:10 +0200

    decided to remove the notification entirely

--- template/js/notification.js
+++ template/js/notification.js
@@ -1,27 +1,28 @@
-navigator.serviceWorker.register('/sw.js');
+navigator.serviceWorker.register('/template/sw.js');
 
-// TODO: Fix 'Thank you' notification appearing after the permission is granted.
+let granted = localStorage.getItem("NotificationPermission");
+
 document.querySelector('#start-btn').addEventListener('click', function () {
-    Notification.requestPermission().then(function (result) {
-        if (result === 'granted') {
-            localStorage.setItem("NotificationPermission", "true");
-            new Notification("Thank you.");
-        } else {
-            localStorage.setItem("NotificationPermission", "false");
-        }
-    });
+    if (!granted) {
+        Notification.requestPermission().then(function (result) {
+            if (result === 'granted') {
+                granted = true;
+                localStorage.setItem("NotificationPermission", "true");
+            } else {
+                localStorage.setItem("NotificationPermission", "false");
+            }
+        });
+    }
 });
 
 function SendNotification() {
     if ('serviceWorker' in navigator) {
-        navigator.serviceWorker.register('/sw.js').then(function (registration) {
+        navigator.serviceWorker.register('/template/sw.js').then(function (registration) {
             console.log('Service Worker registered with scope:', registration.scope);
 
-            document.querySelector('#start-btn').addEventListener('click', function () {
-                registration.showNotification('Custom Notification', {
-                    body: 'This is a custom notification message.',
-                    tag: 'custom-notification'
-                });
+            registration.showNotification('Custom Notification', {
+                body: 'This is a custom notification message.',
+                tag: 'custom-notification'
             });
         }).catch(function (error) {
             console.error('Service Worker registration failed:', error);
